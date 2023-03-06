e95e56ad1bbf7dba2f36d71b6bc5a4061cb8042a
Murad501 <mm845635@gmail.com>
2023-03-06 15:52:57 +0600

    dynamic advertised products show completed

--- src/Pages/Home/AdvertisedProducts/AdvertisedProducts.js
+++ src/Pages/Home/AdvertisedProducts/AdvertisedProducts.js
@@ -6,21 +6,30 @@ import { productProvider } from "../../../Context/ProductContext";
 const AdvertisedProducts = () => {
   const { isDark } = useContext(darkProvider);
   const { products } = useContext(productProvider);
+  const advertisedProducts = products.filter(
+    (product) => product.available && product.advertised
+  );
   return (
-    <div className="mt-10 mb-20 mx-auto">
-      <h1
-        className={`text-4xl font-bold text-center mb-10 ${
-          !isDark && "text-rose-400"
-        }`}
-      >
-        Advertised Products
-      </h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3  justify-center items-center gap-2">
-        {products.map((product) => (
-          <ProductCard key={product._id} product={product}></ProductCard>
-        ))}
-      </div>
-    </div>
+    <>
+      {advertisedProducts.length ? (
+        <div className="mt-10 mb-20 mx-auto">
+          <h1
+            className={`text-4xl font-bold text-center mb-10 ${
+              !isDark && "text-rose-400"
+            }`}
+          >
+            Advertised Products
+          </h1>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3  justify-center items-center gap-2">
+            {advertisedProducts.map((product) => (
+              <ProductCard key={product._id} product={product}></ProductCard>
+            ))}
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
+    </>
   );
 };
 
